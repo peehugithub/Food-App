e7caa9ce6cc1a5f01f6079f1383fed138d4e4c56
Pihu <peehupraveen1@gmail.com>
2023-03-26 01:26:46 +0530

    Final Commit

---
+++ app/src/main/java/com/internshala/myfirstapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.internshala.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.miss_food_relative_layout)
+        println("onCreate called")
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy called")
+    }
+}
